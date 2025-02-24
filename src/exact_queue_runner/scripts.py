184239bccfbf74d58e116bf23292d8930e9d0a8e
Jan Hinrichsen <j.hinrichsen95@gmail.com>
2025-02-24 19:18:48 +0100

    debug

--- src/exact_queue_runner/scripts.py
+++ src/exact_queue_runner/scripts.py
@@ -58,6 +58,6 @@ def destroy(job_id:int):
 @click.argument("image",type=str)
 def remove_results(image:str):
     '''remove plugin results for image(s)'''
-    print("debug")
+    @click.echo("debug")
     logger.info('in remove_results')
     exact_connection.get_image_id('test')

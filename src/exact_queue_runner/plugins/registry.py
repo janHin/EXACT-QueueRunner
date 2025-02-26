41be82dd0fa420b5ac1c12dbc3ac173c0300d1a7
Jan Hinrichsen <j.hinrichsen95@gmail.com>
2025-02-26 19:33:26 +0100

    adding function to load all plugin packages to enable proper registration

--- src/exact_queue_runner/plugins/registry.py
+++ src/exact_queue_runner/plugins/registry.py
@@ -3,6 +3,8 @@ Registry module for Exact plugin implementations
 '''
 #STL imports
 import logging
+import pkgutil
+import importlib
 
 #from ..  mport plugins
 
@@ -18,6 +20,24 @@ logger.debug('in registry package')
 #     # import_module to work without having to do additional modification to
 #     # the name.
 #     return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + ".")
+
+def __load_local_modules():
+
+    n_registered = 0
+    for _ , name, _ in sorted(pkgutil.iter_modules(__path__)):
+        if not name.split('.')[-1].startswith('plugin'):
+            continue
+        try:
+            logger.info('activating plugin %s',name)
+            mod = importlib.import_module(name)
+            __registered_plugins[name] = mod.plugin
+        except Exception as e:
+            raise RuntimeError('+++ Unable to activate plugin: '+name) from e
+        n_registered += 1
+    if n_registered <= 0:
+        raise RuntimeError('registered no plugins')
+
+__load_local_modules()
 
 # def __register_plugins():
 #     n_registered = 0

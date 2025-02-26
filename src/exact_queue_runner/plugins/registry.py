9306cf0ad37f68f4b5f75abc0b2238502bbcbebe
Jan Hinrichsen <j.hinrichsen95@gmail.com>
2025-02-26 19:42:16 +0100

    fixing function to load all plugin packages to enable proper registration

--- src/exact_queue_runner/plugins/registry.py
+++ src/exact_queue_runner/plugins/registry.py
@@ -14,18 +14,19 @@ __registered_plugins = {}
 
 logger.debug('in registry package')
 
-# def __iter_namespace(ns_pkg):
-#     # Specifying the second argument (prefix) to iter_modules makes the
-#     # returned name an absolute name instead of a relative one. This allows
-#     # import_module to work without having to do additional modification to
-#     # the name.
-#     return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + ".")
+def __iter_namespace(ns_pkg):
+    # Specifying the second argument (prefix) to iter_modules makes the
+    # returned name an absolute name instead of a relative one. This allows
+    # import_module to work without having to do additional modification to
+    # the name.
+    return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + ".")
 
 def __load_local_modules():
 
     n_registered = 0
-    for _ , name, _ in sorted(pkgutil.iter_modules(plugins.__path__)):
-        if not name.startswith('plugin'):
+    for _ , name, _ in sorted(__iter_namespace(plugins)):
+        module_name = name.split('.')[-1]
+        if not module_name.startswith('plugin'):
             continue
         try:
             logger.info('activating plugin %s',name)

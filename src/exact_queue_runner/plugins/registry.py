f47b1ac6590ab322630dc8534892c7c8fcb695e3
Jan Hinrichsen <j.hinrichsen95@gmail.com>
2025-02-26 19:48:27 +0100

    moved loading of subpackages to init due to circular import

--- src/exact_queue_runner/plugins/registry.py
+++ src/exact_queue_runner/plugins/registry.py
@@ -3,8 +3,7 @@ Registry module for Exact plugin implementations
 '''
 #STL imports
 import logging
-import pkgutil
-import importlib
+
 
 from .. import plugins
 
@@ -13,32 +12,6 @@ logger = logging.getLogger(__name__)
 __registered_plugins = {}
 
 logger.debug('in registry package')
-
-def __iter_namespace(ns_pkg):
-    # Specifying the second argument (prefix) to iter_modules makes the
-    # returned name an absolute name instead of a relative one. This allows
-    # import_module to work without having to do additional modification to
-    # the name.
-    return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + ".")
-
-def __load_local_modules():
-
-    n_registered = 0
-    for _ , name, _ in sorted(__iter_namespace(plugins)):
-        module_name = name.split('.')[-1]
-        if not module_name.startswith('plugin'):
-            continue
-        try:
-            logger.info('activating plugin %s',name)
-            mod = importlib.import_module(name)
-            __registered_plugins[name] = mod.plugin
-        except Exception as e:
-            raise RuntimeError('+++ Unable to activate plugin: '+name) from e
-        n_registered += 1
-    if n_registered <= 0:
-        raise RuntimeError('registered no plugins')
-
-__load_local_modules()
 
 # def __register_plugins():
 #     n_registered = 0
